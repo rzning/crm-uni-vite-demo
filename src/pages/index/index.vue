2f9ef73a3bd37a3b77cb07e6f5e6d14ca5329088
rzning <rzning@outlook.com>
2022-05-24 19:29:14 +0800

    Initialize uni-vite  project

---
+++ src/pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="content">
+    <view class="text-area">
+      <text class="title">{{ title }}</text>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const title = ref('CRM Mobile Demo')
+</script>
+
+<style>
+.content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+}
+
+.text-area {
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  border: 3px solid goldenrod;
+  border-radius: 12px;
+}
+
+.title {
+  font-size: 36rpx;
+  color: darkgrey;
+}
+</style>
